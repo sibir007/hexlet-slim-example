c2d71d8b210ba600fd2a65e10d3ec1d119187501
Dima Sibirykov <sibiriakoff2006@yandex.ru>
2018-11-09 15:57:35 +0300

    Пейджинг

--- public/index.php
+++ public/index.php
@@ -3,6 +3,29 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 
+
+class Generator1
+{
+    public static function generate($count)
+    {
+        $numbers = range(1, 100);
+        shuffle($numbers);
+
+        $faker = \Faker\Factory::create();
+        $faker->seed(1);
+        $companies = [];
+        for ($i = 0; $i < $count; $i++) {
+            $companies[] = [
+                'id' => $numbers[$i],
+                'name' => $faker->company,
+                'phone' => $faker->phoneNumber
+            ];
+        }
+
+        return $companies;
+    }
+}
+
 $configuration = [
 	'settings' => [
 		'displayErrorDetails' => true,
@@ -11,7 +34,20 @@ $configuration = [
 
 $app = new \Slim\App($configuration);
 
-$app->get('/', function ($request, $response) {
-	return $response->write('Welome to Slim!');
+
+$companies = Generator1::generate(100);
+//print_r($companies[1]);
+// BEGIN (write your solution here)
+$app->get('/companies', function ($request, $response) use ($companies) {
+	echo "in get";
+    $page = $request->getQueryParam('page', 1);
+    $per = $request->getQueryParam('per', 1);
+    $rezCompanyTab = array_slice($companies, ($page-1) * $per, $per);
+    echo "print rezCompanyTab";
+    print_r($rezCompanyTab);
+    $jsonRezCompanyTab = json_encode($rezCompanyTab);
+    return $response->write($jsonRezCompanyTab);
 });
 $app->run();
+// END
+
